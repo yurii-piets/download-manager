e50326c89f0efc60276a46f38337852f8f13ce7a
Yurii Piets <yurii.piets@gmail.com>
2018-01-07 14:40:47 +0100

    filename from content-disposition header

--- src/download.erl
+++ src/download.erl
@@ -12,8 +12,7 @@ begin_download({Link, Dir}, Manager) ->
     {ok, {{_, 200, "OK"}, Headers, Body}} ->
       FileName = create_file_name(Link, Headers),
       save_file(Body, Dir, FileName),
-      Manager ! {finish, {Link, Dir}},
-      inets:stop();
+      Manager ! {finish, {Link, Dir}};
     _ ->
       io:format("Unreconbizable response for link ~p~n", [Link])
   end.
@@ -23,13 +22,18 @@ save_file(Content, Dir, FileName) ->
   file:write_file(Path, Content).
 
 create_file_name(Link, Headers) ->
-  case parser:file_name_from_link(Link) of
-    {extentention, Name} ->
+  case parser:file_name_from_header(Headers) of
+    {exist, Name} ->
       Name;
-    {noextentention, Name} ->
-      ContentType = maps:get(?CONTENT_TYPE, maps:from_list(Headers)),
-      Extension = converter:content_type_to_file_extension(ContentType),
-      Name ++ Extension
+    notexist ->
+      case parser:file_name_from_link(Link) of
+        {extentention, Name} ->
+          Name;
+        {noextentention, Name} ->
+          ContentType = maps:get(?CONTENT_TYPE, maps:from_list(Headers)),
+          Extension = converter:content_type_to_file_extension(ContentType),
+          Name ++ Extension
+      end
   end.
 
 inets_start_for_method(Link) ->
--- src/parser.erl
+++ src/parser.erl
@@ -1,13 +1,23 @@
 -module(parser).
 -compile([export_all]).
 -define(FILE_NAME_PATTERN, ".{1,}\..{1,}").
+-define(ATTACHMENT_FILE_NAME_PATTERN, "filename=\".*\"").
 -define(HTTP_PATTERN, "^http://.*").
 -define(HTTPS_PATTERN, "^https://.*").
 -define(CONTENT_DISPOSITION, "content-disposition").
 
 file_name_from_header(Headers) ->
-  ContentDisposition = maps:get(?CONTENT_DISPOSITION, maps:from_list(Headers)),
-  ContentDisposition.
+  HeadersMap = maps:from_list(Headers),
+  case maps:is_key(?CONTENT_DISPOSITION, HeadersMap) of
+    true ->
+      ContentDisposition = maps:get(?CONTENT_DISPOSITION, HeadersMap),
+      Tokens = string:tokens(ContentDisposition, ";"),
+      FileName = file_name(find_first_in_list("filename", Tokens)),
+      {exist, FileName};
+    false ->
+      notexist
+  end.
+
 
 file_name_from_link(Link) ->
   LinkWithoutParameters = get_first_from_list(string:tokens(Link, "?")),
@@ -21,4 +31,26 @@ file_name_from_link(Link) ->
       {noextentention, Name}
   end.
 
+file_name(FileName) ->
+  case re:run(FileName, ?ATTACHMENT_FILE_NAME_PATTERN) of
+    {match, _} ->
+      NameInScopes = get_second_from_list(string:tokens(FileName, "=")),
+      NameWithOutScopes = string:substr(NameInScopes, 2, string:len(NameInScopes) - 2),
+      NameWithOutScopes;
+    nomatch ->
+      ""
+end.
+
+
 get_first_from_list([H | _]) -> H.
+
+get_second_from_list([_ | [V | _]]) -> V.
+
+find_first_in_list(_, []) -> null;
+find_first_in_list(Key, [H | Tail]) ->
+  case string:str(H, Key) of
+    0 ->
+      find_first_in_list(Key, Tail);
+    _ ->
+      H
+  end.
